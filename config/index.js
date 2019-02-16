35aee4c905128719cd6296377e2f05d5cf4192ca
peterbundgaard <peter@pbundgaard.dk>
2019-02-17 00:50:28 +0100

    changed fonts

--- config/index.js
+++ config/index.js
@@ -5,6 +5,14 @@
 const path = require('path')
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.styl$/,
+        loader: ['style-loader', 'css-loader', 'stylus-loader']
+      }
+    ]
+  },
   dev: {
 
     // Paths
